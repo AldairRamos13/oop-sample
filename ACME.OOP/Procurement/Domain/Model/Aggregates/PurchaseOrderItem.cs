b6a9b19093244bd828cb9922d3b91ac8f0fd9534
Aldair <u20201f051@upc.edu.pe>
2025-09-05 17:47:56 -0500

    feat(purchase-order): add p urchase order item class

---
+++ ACME.OOP/Procurement/Domain/Model/Aggregates/PurchaseOrderItem.cs
@@ -0,0 +1,12 @@
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.Model.ValueObjects;
+
+namespace ACME.OOP.Procurement.Domain.Model.Aggregates;
+
+public class PurchaseOrderItem(ProductId productId, int quantity, Money unitPrice) 
+{
+ public ProductId ProductId { get; } = productId?? throw new ArgumentException(nameof(productId));
+ public int Quantity { get; } = quantity>0? quantity: throw new ArgumentOutOfRangeException(nameof(quantity));
+ public Money UnitPrice { get; } = unitPrice ?? throw new ArgumentNullException(nameof(unitPrice));
+ public Money CalculateItemTotal() => new(UnitPrice.Amount * Quantity, UnitPrice.Currency);
+}
